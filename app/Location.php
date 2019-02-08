b7043eaa7f567409a955bedda3b53a74849ec92c
daniel munn <dannymunn1995@gmail.com>
2019-02-08 08:48:24 +0000

    Added slots relationship to location model

--- app/Location.php
+++ app/Location.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 
 class Location extends Model
 {
@@ -14,5 +15,10 @@ class Location extends Model
     public function users()
     {
         return $this->belongsToMany('App\User', 'user_location');
+    }
+
+    public function slots()
+    {
+        return $this->hasMany('App\Slot', 'location', 'name');
     }
 }
